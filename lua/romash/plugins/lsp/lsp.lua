54d3cc715b9a17d2b228be74b3196170c3ec432a
Roman Konashenkov <konashenkov@obit.ru>
2025-12-24 15:59:36 +0000

    [fix]: eslint resolve imports

--- lua/romash/plugins/lsp/lsp.lua
+++ lua/romash/plugins/lsp/lsp.lua
@@ -15,5 +15,34 @@ return {
     vim.lsp.config("*", {
       capabilities = capabilities,
     })
+
+    -- ←ESLint настройка
+    local root_files = {
+      "eslint.config.js",
+      ".eslintrc*",
+      "package.json"
+    }
+    vim.lsp.config('eslint', {
+      settings = {
+        validate = "on",
+        lintArgs = {
+          "--stdin",
+          "--stdin-filename",
+          vim.api.nvim_buf_get_name(0),
+          "--format", "json",
+        },
+        lintStdin = true,
+        lintDebounce = "250ms",
+        lintIgnoreExitCode = true,
+        useFlatConfig = true, -- для eslint.config.js
+      },
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+      end,
+      root_dir = function(fname)
+        return vim.fs.root(fname, root_files)
+      end,
+    })
+    vim.lsp.enable('eslint')
   end,
 }
